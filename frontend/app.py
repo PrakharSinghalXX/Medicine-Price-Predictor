2c6566a9d3080b0bf1d051d6eff89723975dc61a
PrakharSinghalXX <singhal.prakhar1234@gmail.com>
2026-08-07 01:32:18 +0530

    Update app.py

--- frontend/app.py
+++ frontend/app.py
@@ -4,7 +4,7 @@ import pandas as pd
 import plotly.express as px
 
 
-url = "https://medicine-price-backend-api.onrender.com"
+url = "https://medicine-price-backend.onrender.com"
 
 st.set_page_config(
     page_title="Medicine Price Predictor",
